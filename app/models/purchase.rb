10ca97f238a9c333316cac4172553f7b78b948ab
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2015-09-13 02:11:56 +0600

    validation for purchases added

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,6 @@
 class Purchase < ActiveRecord::Base
+  validates :seller_id, presence: true
+  validates :buyer_id, presence: true
+  validates :item_id, presence: true
+  validates :price, presence: true
 end
