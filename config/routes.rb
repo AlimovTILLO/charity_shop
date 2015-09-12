260fdb0c3e8efd023374211f28ae8041cc99cd94
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2015-09-12 23:11:47 +0600

    new admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :administrators, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
   resources :items
   root 'home#index'
