559af6e951319c7a1454634e52222b6d74ed25a1
Aidan <aidanxyz@gmail.com>
2015-09-13 11:58:23 +0600

    top nav bar improved

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912201530) do
+ActiveRecord::Schema.define(version: 20150912195220) do
 
   create_table "administrators", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -49,9 +49,8 @@ ActiveRecord::Schema.define(version: 20150912201530) do
     t.string   "description"
     t.integer  "price"
     t.string   "thumbnail"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "foundation_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "purchases", force: :cascade do |t|
