94d452d04656d7e4eabc1cf9c60beea1db05e4fc
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2015-09-13 09:57:50 +0600

    permit foundation_id for items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -70,6 +70,11 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:user_id, :title, :description, :price, :thumbnail)
+      params.require(:item).permit :user_id,
+                                   :title,
+                                   :description,
+                                   :price,
+                                   :thumbnail,
+                                   :foundations_id
     end
 end
